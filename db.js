972ba8f35230312212ba4da76dcd06e58039c956
jhamile17 <i2414018@continental.edu.pe>
2025-09-20 00:30:08 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root', // Cambia esto por tu usuario de MySQL
-  password: '', // Cambia esto por tu contraseña de MySQL
-  database: 'tienda', // Cambia esto por el nombre de tu base de datos
+  host: process.env.DB_HOST,       // Host de la DB en Railway
+  user: process.env.DB_USER,       // Usuario de la DB
+  password: process.env.DB_PASSWORD, // Contraseña
+  database: process.env.DB_NAME,   // Nombre de la DB
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
